b2e4bf869dd7287a12b11808fde6d5f836fddc68
cnd86038-ai <cnd86038@email.vccs.edu>
2026-04-06 15:31:58 -0400

    Setup using string for the names, input file, and for first and last student. Opening the file to view the names.

---
+++ p2. Student Lineup/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ifstream inputFile("lineup.txt");
+	string name;
+	string firstStudent;
+	string LastStudent;
+	int count = 0;
+
+	//Open the file
+	inputFile.open("lineup.txt");
+}
