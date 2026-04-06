0ec6cc3db55e4a06fe890054db1ca78af6962f35
cnd86038-ai <cnd86038@email.vccs.edu>
2026-04-06 15:33:03 -0400

    Exit for if some reason the input file "lineup.txt" can't open, it will give an error code and notify why the program failed.

--- p2. Student Lineup/main.cpp
+++ p2. Student Lineup/main.cpp
@@ -13,4 +13,11 @@ int main()
 
 	//Open the file
 	inputFile.open("lineup.txt");
+
+	//Check if the file opened successfully
+	if (!inputFile)
+	{
+		cout << "Error opening file!" << endl;
+		return 1; // Exit with an error code
+	}
 }
